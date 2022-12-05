7de90e5260042ccfd72d2e9ab6f08f73523fdca7
N00bFlesh <andreas.van.vooren@gmail.com>
2022-12-05 20:29:18 +0100

    On the fifth day of Advent, the algo gave to me:
               OOOOOOOOOO OOOOOOOOOO OO      OO OOOOOOOOOO
               OOOOOOOOOO OOOOOOOOOO OO      OO OOOOOOOOOO
               OO             OO     OO      OO OO
               OO             OO     OOO    OOO OO
               OOOOOOOOOO     OO     OOO    OOO OOOOOOOOOO
               OOOOOOOOOO     OO      OO    OO  OOOOOOOOOO
               OO             OO      OOO  OOO  OO
               OO             OO      OOO  OOO  OO
               OO         OOOOOOOOOO   OOOOOO   OOOOOOOOOO
               OO         OOOOOOOOOO    OOOO    OOOOOOOOOO
    
      OOOOOO     OOOOOO   OO         OOOOOOOO   OOOOOOOOOO OO      OO
     OOOOOOOO   OOOOOOOO  OO         OOOOOOOOO  OOOOOOOOOO OOO     OO
    OOO    OOO OOO    OOO OO         OO    OOOO OO         OOOO    OO
    OO      OO OO      OO OO         OO     OOO OO         OOOOO   OO
    OO         OO      OO OO         OO      OO OOOOOOOOOO OO OOO  OO
    OO  OOOOOO OO      OO OO         OO      OO OOOOOOOOOO OO  OOOOOO
    OO     OOO OO      OO OO         OO     OOO OO         OO   OOOOO
    OOO    OOO OOO    OOO OO         OO    OOOO OO         OO    OOOO
     OOOOOOOO   OOOOOOOO  OOOOOOOOOO OOOOOOOOO  OOOOOOOOOO OO     OOO
      OOOOOO     OOOOOO   OOOOOOOOOO OOOOOOOO   OOOOOOOOOO OO      OO
    
          OOOOOOOOO  OOOOOOOOOO OO      OO   OOOOOO    OOOOOOOO
          OOOOOOOOOO OOOOOOOOOO OOO     OO  OOOOOOOO  OOOOOOOOOO
          OO      OO     OO     OOOO    OO OOO    OOO OO      OO
          OO      OO     OO     OOOOO   OO OO      OO OOO
          OOOOOOOOOO     OO     OO OOO  OO OO         OOOOOOOOO
          OOOOOOOOO      OO     OO  OOOOOO OO  OOOOOO  OOOOOOOOO
          OO   OO        OO     OO   OOOOO OO     OOO        OOO
          OO    OO       OO     OO    OOOO OOO    OOO OO      OO
          OO     OO  OOOOOOOOOO OO     OOO  OOOOOOOO  OOOOOOOOOO
          OO      OO OOOOOOOOOO OO      OO   OOOOOO    OOOOOOOO
    
    Four cleaning brooms;
    Three messy bags;
    Two filthy cheats;
    And an overdose of calories.

--- Day05/Day05.cpp
+++ Day05/Day05.cpp
@@ -1,7 +1,132 @@
 #include "Day05.h"
+
+#include <algorithm>
+#include <vector>
+#include <regex>
+#include <ranges>
+#include "../Utils.h"
+
 std::string Day05::GetResultStr()
 {
-	return "---Day05 is not yet implemented! ---";
+	std::stringstream resultStream{};
+	std::vector<std::vector<char>> warehouse{};
+	bool bHasFilledWarehouse = false;
+	bool bIsCrateMover9001 = false;
+
+	const auto stackTop = [&warehouse]() -> std::string
+	{
+		std::stringstream stream{};
+		for (const auto& stack : warehouse)
+		{
+			if (stack.size() > 0)
+			{
+				stream << *stack.crbegin();
+			}
+		}
+		return stream.str();
+	};
+	const auto reset = [&]
+	{
+		warehouse = {};
+		bHasFilledWarehouse = false;
+	};
+	const auto output = [&](const std::string& debug)
+	{
+		resultStream << "Stack top [" << debug <<"]: " << stackTop() << "\n";
+	};
+	const auto addLineToWarehouse = [&](const std::string& line)
+	{
+		if (line.empty())
+		{
+			for (auto& stack : warehouse)
+			{
+				// Pop the back since that's the index.
+				stack.pop_back();
+				std::reverse(stack.begin(), stack.end());
+			}
+			bHasFilledWarehouse = true;
+		}
+		else
+		{
+			// The input test file is filled with spaces where no element is present.
+			size_t numColumns = (line.length() + 3) / 4;
+			if (warehouse.size() == 0)
+			{
+				warehouse.resize(numColumns);
+			}
+			for (size_t i = 0; i < numColumns; ++i)
+			{
+				size_t charIdx = (i * 4) + 1;
+				// Warehouse is in format [A] [B] [C]
+				// -> start at 1 for A] [B] [C]
+				// -> advance 4 for B] [C]
+				if (line[charIdx] != ' ')
+				{
+					warehouse[i].push_back(line[charIdx]);
+				}
+			}
+		}
+	};
+	const auto processCmd = [&](const std::string& line)
+	{
+		if (line.empty()) return;
+		const std::regex regexObj{"move ([0-9]+) from ([0-9]+) to ([0-9]+)"};
+		std::smatch match;
+		if (std::regex_match(line, match, regexObj))
+		{
+			// The first sub_match is the whole string; the next
+			// sub_match is the first parenthesized expression.
+			const int src = stoi(match[2].str()) - 1;
+			const int dst = stoi(match[3].str()) - 1;
+			// We're adding the final row to the columns, which is just a bunch of indices.
+			const int num = stoi(match[1].str());
+			if (num > warehouse[src].size())
+			{
+				__debugbreak();
+			}
+			if (bIsCrateMover9001)
+			{
+				warehouse[dst].insert(warehouse[dst].end(), std::prev(warehouse[src].cend(), num), warehouse[src].cend());
+			}
+			else
+			{
+				warehouse[dst].insert(warehouse[dst].end(), warehouse[src].crbegin(), std::next(warehouse[src].crbegin(), num));
+			}
+			warehouse[src].resize(warehouse[src].size() - num);
+		}
+	};
+	const auto processLambda = [&](const std::string& line)
+	{
+		if (!bHasFilledWarehouse)
+		{
+			addLineToWarehouse(line);
+		}
+		else
+		{
+			processCmd(line);
+		}
+	};
+
+	reset();
+	ForEachLineInTestInputFile(processLambda);
+	output("Test");
+	if (stackTop() != "CMZ") __debugbreak();
+	reset();
+	ForEachLineInInputFile(processLambda);
+	output("Final");
+
+	bIsCrateMover9001 = true;
+
+	reset();
+	ForEachLineInTestInputFile(processLambda);
+	output("Test9001");
+	if (stackTop() != "MCD") __debugbreak();
+	reset();
+	ForEachLineInInputFile(processLambda);
+	output("Final9001");
+
+
+	return resultStream.str();
 };
 std::string Day05::GetIdStr()
 {
