5363b73941208743192db225debc41aefed60734
N00bFlesh <andreas.van.vooren@gmail.com>
2022-12-06 19:20:40 +0100

    On the fifth day of Advent, the algo gave to me: Six walkie-talkies;
               OOOOOOOOOO OOOOOOOOOO OO      OO OOOOOOOOOO
               OOOOOOOOOO OOOOOOOOOO OO      OO OOOOOOOOOO
               OO             OO     OO      OO OO
               OO             OO     OOO    OOO OO
               OOOOOOOOOO     OO     OOO    OOO OOOOOOOOOO
               OOOOOOOOOO     OO      OO    OO  OOOOOOOOOO
               OO             OO      OOO  OOO  OO
               OO             OO      OOO  OOO  OO
               OO         OOOOOOOOOO   OOOOOO   OOOOOOOOOO
               OO         OOOOOOOOOO    OOOO    OOOOOOOOOO
    
      OOOOOO     OOOOOO   OO         OOOOOOOO   OOOOOOOOOO OO      OO
     OOOOOOOO   OOOOOOOO  OO         OOOOOOOOO  OOOOOOOOOO OOO     OO
    OOO    OOO OOO    OOO OO         OO    OOOO OO         OOOO    OO
    OO      OO OO      OO OO         OO     OOO OO         OOOOO   OO
    OO         OO      OO OO         OO      OO OOOOOOOOOO OO OOO  OO
    OO  OOOOOO OO      OO OO         OO      OO OOOOOOOOOO OO  OOOOOO
    OO     OOO OO      OO OO         OO     OOO OO         OO   OOOOO
    OOO    OOO OOO    OOO OO         OO    OOOO OO         OO    OOOO
     OOOOOOOO   OOOOOOOO  OOOOOOOOOO OOOOOOOOO  OOOOOOOOOO OO     OOO
      OOOOOO     OOOOOO   OOOOOOOOOO OOOOOOOO   OOOOOOOOOO OO      OO
    
          OOOOOOOOO  OOOOOOOOOO OO      OO   OOOOOO    OOOOOOOO
          OOOOOOOOOO OOOOOOOOOO OOO     OO  OOOOOOOO  OOOOOOOOOO
          OO      OO     OO     OOOO    OO OOO    OOO OO      OO
          OO      OO     OO     OOOOO   OO OO      OO OOO
          OOOOOOOOOO     OO     OO OOO  OO OO         OOOOOOOOO
          OOOOOOOOO      OO     OO  OOOOOO OO  OOOOOO  OOOOOOOOO
          OO   OO        OO     OO   OOOOO OO     OOO        OOO
          OO    OO       OO     OO    OOOO OOO    OOO OO      OO
          OO     OO  OOOOOOOOOO OO     OOO  OOOOOOOO  OOOOOOOOOO
          OO      OO OOOOOOOOOO OO      OO   OOOOOO    OOOOOOOO
    
    Four cleaning brooms;
    Three messy bags;
    Two filthy cheats;
    And an overdose of calories.

--- Day06/Day06.cpp
+++ Day06/Day06.cpp
@@ -1,7 +1,57 @@
 #include "Day06.h"
+
+#include <algorithm>
+#include <vector>
+#include <regex>
+#include <ranges>
+#include "../Utils.h"
+
 std::string Day06::GetResultStr()
 {
-	return "---Day06 is not yet implemented! ---";
+	std::stringstream resultStream{};
+	std::string debug{};
+	const auto processLambda = [&](const std::string& line)
+	{
+		if (line.empty()) { return; }
+		size_t packetStartIndex = static_cast<size_t>(-1);
+		size_t msgStartIndex = static_cast<size_t>(-1);
+		for (size_t charIdx = 0; (charIdx < line.length() - 13); ++charIdx)
+		{
+			bool bInvalidPacketStart = false;
+			bool bInvalidMsgStart = false;
+			for (size_t i = charIdx; i < charIdx + 14 && i < line.length(); ++i)
+			{
+				for (size_t j = i + 1; j < charIdx + 14 && j < line.length() ; j++)
+				{
+					if (line[i] == line[j])
+					{
+						if ((j - charIdx) < 4)
+						{
+							bInvalidPacketStart = true;
+						}
+						bInvalidMsgStart = true;
+					}
+				}
+			}
+
+			if (!bInvalidPacketStart && packetStartIndex == static_cast<size_t>(-1))
+			{
+				packetStartIndex = charIdx + 4;
+			}
+			if (!bInvalidMsgStart && msgStartIndex == static_cast<size_t>(-1))
+			{
+				msgStartIndex = charIdx + 14;
+			}
+		}
+
+		resultStream << "[" << debug << "] Start of packet = " << packetStartIndex << ", Start of msg = " << msgStartIndex << "\n";
+	};
+	debug = "test";
+	ForEachLineInTestInputFile(processLambda);
+	debug = "realsies";
+	ForEachLineInInputFile(processLambda);
+
+	return resultStream.str();
 };
 std::string Day06::GetIdStr()
 {
