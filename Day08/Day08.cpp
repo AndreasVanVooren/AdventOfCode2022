3674ef5488c961cc376bda4934ee5dcca746c35b
N00bFlesh <andreas.van.vooren@gmail.com>
2022-12-08 19:22:27 +0100

    On the eighth day of Advent, the algo gave to me: Eight planks of treehouse; Seven storage drives; Six walkie-talkies;
               OOOOOOOOOO OOOOOOOOOO OO      OO OOOOOOOOOO
               OOOOOOOOOO OOOOOOOOOO OO      OO OOOOOOOOOO
               OO             OO     OO      OO OO
               OO             OO     OOO    OOO OO
               OOOOOOOOOO     OO     OOO    OOO OOOOOOOOOO
               OOOOOOOOOO     OO      OO    OO  OOOOOOOOOO
               OO             OO      OOO  OOO  OO
               OO             OO      OOO  OOO  OO
               OO         OOOOOOOOOO   OOOOOO   OOOOOOOOOO
               OO         OOOOOOOOOO    OOOO    OOOOOOOOOO
    
      OOOOOO     OOOOOO   OO         OOOOOOOO   OOOOOOOOOO OO      OO
     OOOOOOOO   OOOOOOOO  OO         OOOOOOOOO  OOOOOOOOOO OOO     OO
    OOO    OOO OOO    OOO OO         OO    OOOO OO         OOOO    OO
    OO      OO OO      OO OO         OO     OOO OO         OOOOO   OO
    OO         OO      OO OO         OO      OO OOOOOOOOOO OO OOO  OO
    OO  OOOOOO OO      OO OO         OO      OO OOOOOOOOOO OO  OOOOOO
    OO     OOO OO      OO OO         OO     OOO OO         OO   OOOOO
    OOO    OOO OOO    OOO OO         OO    OOOO OO         OO    OOOO
     OOOOOOOO   OOOOOOOO  OOOOOOOOOO OOOOOOOOO  OOOOOOOOOO OO     OOO
      OOOOOO     OOOOOO   OOOOOOOOOO OOOOOOOO   OOOOOOOOOO OO      OO
    
          OOOOOOOOO  OOOOOOOOOO OO      OO   OOOOOO    OOOOOOOO
          OOOOOOOOOO OOOOOOOOOO OOO     OO  OOOOOOOO  OOOOOOOOOO
          OO      OO     OO     OOOO    OO OOO    OOO OO      OO
          OO      OO     OO     OOOOO   OO OO      OO OOO
          OOOOOOOOOO     OO     OO OOO  OO OO         OOOOOOOOO
          OOOOOOOOO      OO     OO  OOOOOO OO  OOOOOO  OOOOOOOOO
          OO   OO        OO     OO   OOOOO OO     OOO        OOO
          OO    OO       OO     OO    OOOO OOO    OOO OO      OO
          OO     OO  OOOOOOOOOO OO     OOO  OOOOOOOO  OOOOOOOOOO
          OO      OO OOOOOOOOOO OO      OO   OOOOOO    OOOOOOOO
    
    Four cleaning brooms;
    Three messy bags;
    Two filthy cheats;
    And an overdose of calories.

--- Day08/Day08.cpp
+++ Day08/Day08.cpp
@@ -1,7 +1,120 @@
 #include "Day08.h"
+
+#include <algorithm>
+#include <filesystem>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <regex>
+#include <ranges>
+#include "../Utils.h"
+
 std::string Day08::GetResultStr()
 {
-	return "---Day08 is not yet implemented! ---";
+	std::stringstream resultStream{};
+
+	std::vector<std::vector<int>> treeGrid{};
+
+	const auto reset = [&]
+	{
+		treeGrid = {};
+	};
+
+	const auto fetchTreeRow = [&](const std::string& line)
+	{
+		if (line.empty()) return;
+		std::vector<int> temp{};
+		for (const auto& c : line)
+		{
+			if (c >= '0' && c <= '9')
+			{
+				temp.push_back(c - '0');
+			}
+		}
+		treeGrid.push_back(std::move(temp));
+	};
+
+	const auto getTreeStats = [&](const size_t rowIndex, const size_t colIndex)
+	{
+		const auto getTreeStatIter = [](const auto& begin, const auto& end, const auto& pred)
+		{
+			const auto encounter = std::find_if(begin, end, pred);
+			const ptrdiff_t distance = std::distance(begin, encounter);
+			const bool bVisible = distance == std::distance(begin, end);
+			const ptrdiff_t scenicScore = bVisible ? distance : (distance + 1);
+			return std::make_pair(bVisible, scenicScore);
+		};
+		const auto [bVisibleFromLeft, scenicScoreLeft] = getTreeStatIter(
+			  std::prev(treeGrid[rowIndex].crend(), colIndex)
+			, treeGrid[rowIndex].crend()
+			, [&](const int& tree) {return treeGrid[rowIndex][colIndex] <= tree; }
+		);
+		const auto [bVisibleFromRight, scenicScoreRight] = getTreeStatIter(
+			  std::next(treeGrid[rowIndex].cbegin(), colIndex + 1)
+			, treeGrid[rowIndex].cend()
+			, [&](const int& tree) {return treeGrid[rowIndex][colIndex] <= tree; }
+		);
+		const auto [bVisibleFromTop, scenicScoreTop] = getTreeStatIter(
+			  std::prev(treeGrid.crend(), rowIndex)
+			, treeGrid.crend()
+			, [&](const std::vector<int>& treeRow) {return treeGrid[rowIndex][colIndex] <= treeRow[colIndex]; }
+		);
+		const auto [bVisibleFromBot, scenicScoreBot] = getTreeStatIter(
+			  std::next(treeGrid.cbegin(), rowIndex + 1)
+			, treeGrid.cend()
+			, [&](const std::vector<int>& treeRow) {return treeGrid[rowIndex][colIndex] <= treeRow[colIndex]; }
+		);
+
+		const bool bIsVisible = bVisibleFromBot || bVisibleFromLeft || bVisibleFromRight || bVisibleFromTop;
+		const ptrdiff_t scenicScore = scenicScoreLeft * scenicScoreRight * scenicScoreTop * scenicScoreBot;
+		return std::make_pair( bIsVisible, scenicScore );
+	};
+
+	const auto getNumVisibleTrees = [&]
+	{
+		size_t num = 0;
+		ptrdiff_t highestScenicScore = 0;
+		size_t highestRow = std::numeric_limits<size_t>::max();
+		size_t highestCol = std::numeric_limits<size_t>::max();
+		for (size_t row = 0; row < treeGrid.size(); ++row)
+		{
+			for (size_t col = 0; col < treeGrid[row].size(); ++col)
+			{
+				const auto [visible, score] = getTreeStats(row, col);
+				if (score > highestScenicScore)
+				{
+					highestScenicScore = score;
+					highestRow = row;
+					highestCol = col;
+				}
+				if (visible)
+				{
+					++num;
+				}
+			}
+		}
+		return std::make_tuple(num, highestScenicScore, highestRow, highestCol);
+	};
+
+	reset();
+	ForEachLineInTestInputFile(fetchTreeRow);
+	{
+		const auto [numVisible, highestScenic, row, col] = getNumVisibleTrees();
+		resultStream << "Test input returned " << numVisible << " trees visible, with a highest scenic score of " << highestScenic << " at [" << col << "," << row << "]\n";
+		if (numVisible != 21) __debugbreak();
+		if (highestScenic != 8) __debugbreak();
+		if (col != 2) __debugbreak();
+		if (row != 3) __debugbreak();
+	}
+
+	reset();
+	ForEachLineInInputFile(fetchTreeRow);
+	{
+		const auto [numVisible, highestScenic, row, col] = getNumVisibleTrees();
+		resultStream << "Input returned " << numVisible << " trees visible, with a highest scenic score of " << highestScenic << " at [" << col << "," << row << "]\n";
+	}
+
+	return resultStream.str();
 };
 std::string Day08::GetIdStr()
 {
