811e9704e11c0d489e765bbab29c617f139ed274
N00bFlesh <andreas.van.vooren@gmail.com>
2022-12-20 22:12:29 +0100

    On the 20th day of Advent, the algo gave to me: Twenty Grove street homies; Nineteen digger robots; Eighteen lava droplets; Seventeen trillion rocks; Sixteen travelling salesmen; Fifteen beeping beacons; Fourteen entering sandmen; Thirteen distress signals; Twelve mountain boots; Eleven flipping monkeys; Ten cathode-ray tubes; Nine quantum physics; Eight planks of treehouse; Seven storage drives; Six walkie-talkies;
               OOOOOOOOOO OOOOOOOOOO OO      OO OOOOOOOOOO
               OOOOOOOOOO OOOOOOOOOO OO      OO OOOOOOOOOO
               OO             OO     OO      OO OO
               OO             OO     OOO    OOO OO
               OOOOOOOOOO     OO     OOO    OOO OOOOOOOOOO
               OOOOOOOOOO     OO      OO    OO  OOOOOOOOOO
               OO             OO      OOO  OOO  OO
               OO             OO      OOO  OOO  OO
               OO         OOOOOOOOOO   OOOOOO   OOOOOOOOOO
               OO         OOOOOOOOOO    OOOO    OOOOOOOOOO
    
      OOOOOO     OOOOOO   OO         OOOOOOOO   OOOOOOOOOO OO      OO
     OOOOOOOO   OOOOOOOO  OO         OOOOOOOOO  OOOOOOOOOO OOO     OO
    OOO    OOO OOO    OOO OO         OO    OOOO OO         OOOO    OO
    OO      OO OO      OO OO         OO     OOO OO         OOOOO   OO
    OO         OO      OO OO         OO      OO OOOOOOOOOO OO OOO  OO
    OO  OOOOOO OO      OO OO         OO      OO OOOOOOOOOO OO  OOOOOO
    OO     OOO OO      OO OO         OO     OOO OO         OO   OOOOO
    OOO    OOO OOO    OOO OO         OO    OOOO OO         OO    OOOO
     OOOOOOOO   OOOOOOOO  OOOOOOOOOO OOOOOOOOO  OOOOOOOOOO OO     OOO
      OOOOOO     OOOOOO   OOOOOOOOOO OOOOOOOO   OOOOOOOOOO OO      OO
    
          OOOOOOOOO  OOOOOOOOOO OO      OO   OOOOOO    OOOOOOOO
          OOOOOOOOOO OOOOOOOOOO OOO     OO  OOOOOOOO  OOOOOOOOOO
          OO      OO     OO     OOOO    OO OOO    OOO OO      OO
          OO      OO     OO     OOOOO   OO OO      OO OOO
          OOOOOOOOOO     OO     OO OOO  OO OO         OOOOOOOOO
          OOOOOOOOO      OO     OO  OOOOOO OO  OOOOOO  OOOOOOOOO
          OO   OO        OO     OO   OOOOO OO     OOO        OOO
          OO    OO       OO     OO    OOOO OOO    OOO OO      OO
          OO     OO  OOOOOOOOOO OO     OOO  OOOOOOOO  OOOOOOOOOO
          OO      OO OOOOOOOOOO OO      OO   OOOOOO    OOOOOOOO
    
    Four cleaning brooms;
    Three messy bags;
    Two filthy cheats;
    And an overdose of calories.

--- Day20/Day20.cpp
+++ Day20/Day20.cpp
@@ -1,9 +1,123 @@
 #include "Day20.h"
-std::wstring Day20::GetResultStr()
+
+#include <string_view>
+#include <regex>
+#include <numeric>
+#include <unordered_set>
+
+#include "../Utils.h"
+#include "../ScopeProfiler.h"
+
+using namespace std;
+
+wstring Day20::GetResultStr()
 {
-	return L"---Day20 is not yet implemented! ---\n";
+	wstringstream resultStream{};
+	vector<int64_t> numberList{};
+
+	const auto reset = [&] { numberList = {}; };
+
+	const auto parseLine = [&](const wstring_view& line)
+	{
+		if (line.empty()) return;
+		numberList.push_back(static_cast<int64_t>(stoi(line)));
+	};
+
+	const auto getDecrypted = [&](int64_t multiplier, size_t mixAmount)
+	{
+		vector<int64_t> workingList = numberList;
+		if (multiplier != 1)
+		{
+			for (int64_t& num : workingList)
+			{
+				num *= multiplier;
+			}
+		}
+		vector<size_t> indexList{};
+		indexList.resize(numberList.size());
+		iota(indexList.begin(), indexList.end(), 0);
+
+		for (size_t mix = 0; mix < mixAmount; mix++)
+		{
+			for (size_t i = 0; i < numberList.size(); i++)
+			{
+				int64_t placesToMove = numberList[i] * multiplier;
+				placesToMove
+					= (placesToMove > 0)
+					? (placesToMove % (numberList.size() - 1))
+					: -((-placesToMove) % static_cast<int64_t>(numberList.size() - 1));
+
+				if (placesToMove == 0) continue;
+
+				const vector<size_t>::const_iterator iter = ranges::find(indexList, i);
+				ptrdiff_t index = distance(indexList.cbegin(), iter);
+				for (int64_t j = 0; j < placesToMove; j++)
+				{
+					ptrdiff_t nextIndex = (index + 1) % numberList.size();
+					std::swap(workingList[index], workingList[nextIndex]);
+					std::swap(indexList[index], indexList[nextIndex]);
+					index = nextIndex;
+				}
+				for (int64_t j = 0; j > placesToMove; j--)
+				{
+					ptrdiff_t prevIndex = (index > 0) ? (index - 1) : (numberList.size() - 1);
+					std::swap(workingList[index], workingList[prevIndex]);
+					std::swap(indexList[index], indexList[prevIndex]);
+					index = prevIndex;
+				}
+			}
+		}
+		return workingList;
+	};
+	const auto getNumAtOffset = [](const vector<int64_t>& vec, int offset)
+	{
+		const vector<int64_t>::const_iterator zeroIter = ranges::find(vec, 0);
+		const ptrdiff_t zeroIndex = distance(vec.cbegin(), zeroIter);
+		const ptrdiff_t trueIndex = (zeroIndex + offset) % vec.size();
+		return vec[trueIndex];
+	};
+	const auto getGroveCoordinateSum = [&](size_t multiplier = 1, size_t mixAmount = 1)
+	{
+		ScopeProfiler prof{ L"GroveCoordinates " + to_wstring(multiplier) + L" - " + to_wstring(mixAmount)};
+		const vector<int64_t> workingList = getDecrypted(multiplier, mixAmount);
+		const int64_t sum
+			= getNumAtOffset(workingList, 1000)
+			+ getNumAtOffset(workingList, 2000)
+			+ getNumAtOffset(workingList, 3000);
+		return sum;
+	};
+
+	const auto output = [&](const int64_t result, const int64_t result2, const wstring& debug)
+	{
+		resultStream << L"[" << debug << L"] Grove coordinate sum = " << result << ", with proper decryption = " << result2 << "\n";
+	};
+
+	reset();
+	ForEachLineInTestInputFile(parseLine);
+	auto sum = getGroveCoordinateSum();
+	auto sum2 = getGroveCoordinateSum(811589153, 10);
+	if (sum != 3) __debugbreak();
+	if (sum2 != 1623178306) __debugbreak();
+	output(sum,sum2, L"Test");
+
+
+	reset();
+	numberList = { 0,2,-4,3,6,3,7,-1,-3 };
+	sum = getGroveCoordinateSum();
+	sum2 = getGroveCoordinateSum(811589153, 10);
+	if (sum != 12) __debugbreak();
+
+	reset();
+	ForEachLineInInputFile(parseLine);
+	sum = getGroveCoordinateSum();
+	sum2 = getGroveCoordinateSum(811589153, 10);
+	if (sum != 5904) __debugbreak();
+	if (sum2 <= 4355798984151) __debugbreak();
+	output(sum, sum2, L"Final");
+
+	return resultStream.str();
 };
-std::wstring Day20::GetIdStr()
+wstring Day20::GetIdStr()
 {
 	return L"Day20";
 };
